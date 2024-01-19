e23de6f742b0dc02b7ad8bb0b1cd86ad565508be
barnaLeo <156253946+barnaLeo@users.noreply.github.com>
2024-01-19 23:54:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].style.display = "none";
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = "block";
+}
